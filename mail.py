881c5dd4fb59d1fc806d3623c3bc907e97faba8c
Serg <monergeim@gmail.com>
2019-09-19 18:12:32 +0300

    Create mail.py

---
+++ mail.py
@@ -0,0 +1,46 @@
+import os
+#import io
+import smtplib
+from string import Template
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#read mail body file
+def read_template(filename):
+  with open(filename, 'r', encoding='utf-8') as template_file:
+    template_file_content = template_file.read()
+  return Template(template_file_content)
+
+#assigning env_variables from job	
+SENDER_ADDR = os.environ['SENDER_ADDRESS']
+RECEIVER = os.environ['RECEIVER_ADDRESS']
+SMTP_ADDR = os.environ['SMTP_SERVER']
+
+def main():
+  message = read_template('output.html')
+
+# set up the SMTP server
+  s = smtplib.SMTP(SMTP_ADDR)
+  s.set_debuglevel(1)
+  s.ehlo()
+
+# create a message
+  msg = MIMEMultipart()       
+
+# setup the parameters of the message
+  msg['From']=SENDER_ADDR
+  msg['To']=", ".join(RECEIVER)
+  msg['Subject']="[SPARTA] Navigation releasecandidate build"
+
+# add in the message body
+  msg.attach(MIMEText(message, 'html'))
+
+# send the message via the server set up earlier.
+  s.send_message(msg)
+    
+  del msg
+  
+s.quit()
+
+if __name__ == '__main__':
+  main()
