c21c63549b28cb7e571d6db7baf409911b99e4cb
Serg Li <monergeim@gmail.com>
2021-01-29 20:49:10 +0200

    correct comeback

---
+++ tmp.py
@@ -0,0 +1,18 @@
+print("World.\nHold on. Ta-ta-ta-tata")
+
+a=10
+b="Hi"
+name="mr.serg li"
+
+print(str(a) + "\n" + b)
+print(name.upper())
+print(b.lower())
+print(name.title())
+x=0
+
+for i in range(0,4):
+    x+=1
+    if i==2:
+        continue
+    print(i)
+    print("x="+str(x))
