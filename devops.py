f44cecf141f3407cc31d83da7152085741a8a2ea
monergeim <monergeim@gmail.com>
2021-02-13 23:59:19 +0200

    devops way

--- devops.py
+++ devops.py
@@ -1,5 +1,14 @@
-#!/usr/bin/python
+#!/usr/local/bin/python3
 
 for i in range(10):
   x = i
   print(x)
+
+count = 0
+
+while True:
+#  print("The count is {}".format(count))
+  print(f"The count is {count}")
+  if count > 5:
+    break
+  count += 1
