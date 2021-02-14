f6b0fadabe72d321c0f0bc244989c45ce28096bd
monergeim <monergeim@gmail.com>
2021-02-14 23:12:32 +0200

    devops lists and strings

--- devops.py
+++ devops.py
@@ -12,3 +12,42 @@ while True:
   if count > 5:
     break
   count += 1
+
+my_sequence = "Bill Cheatham"
+my_sequence.index('C')
+my_sequence.index('a',9, 12) #search within range
+my_sequence[2]
+
+my_sequence = [0, 1, 2, 0, 1, 2, 3, 0, 1, 2, 3, 4]
+len(my_sequence)
+min(my_sequence)
+max(my_sequence)
+my_sequence.count(1)
+#Lists
+a = list()
+b = list(range(10))
+c = list("Henry Miller")
+nine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+nine.append('rhubarb')
+nine.insert(1, 'cream')
+b.extend(c)
+
+squares = [i*i for i in range(10)]
+squares = [i*i for i in range(10) if i%2==0]
+#Strings
+multi_line = ''' Print
+multi
+line
+string'''
+print(multi_line)
+url = "gt.motomomo.io/v2/api/asset/143"
+url.split('/')
+items = ['cow', 'milk', 'bread', 'butter']
+print(" and ".join(items)
+name = "John Paul Jones"
+name.capitalize())
+name.upper()
+name.title()
+name.swapcase()
+
