4dd753d12a66c3e5dc05352704113575e632dc81
Sergii Lypnytskyi <monergeim@gmail.com>
2021-03-02 17:42:27 +0200

    Update web_req.py

--- web_req.py
+++ web_req.py
@@ -5,17 +5,18 @@ import requests
 
 w_link = sys.argv[1]
 x = requests.get(w_link)
+line = 7257
 
 
-#print(x.text)
-
-with open('Output_new.txt', 'w') as file:
-    file.write(x.text)
-    file.close()
+with open("Output_new.txt", "w") as text_file:
+    print(x.text, file=text_file)
 
 with open('Output.txt') as f1, open('Output_new.txt') as f2:
-    for line1, line2 in zip(f1, f2):
-        if line1 != line2:
-            print("Fail")
-            print(line1)
-            print(line2)
+    l1=f1.readlines()
+    l2=f2.readlines()
+#    print(l1[line])
+#    print(l2[line])
+    if l1[line] != l2[line]:
+        print('Fail')
+        with open("Output.txt", "w") as text_file:
+            print(x.text, file=text_file)
