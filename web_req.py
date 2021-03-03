1db62688606b21c4823ed1436a47ca877439d901
Serg Li <monergeim@gmail.com>
2021-03-03 20:44:52 +0200

    website check automation

--- web_req.py
+++ web_req.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import requests
 from subprocess import call
 
@@ -8,6 +9,8 @@ w_link = sys.argv[1]
 x = requests.get(w_link)
 line = 7257
 message = "website has changes"
+path = "/home/sergiilypnytskyi/git/python"
+os.chdir(path)
 
 with open("Output_new.txt", "w") as text_file:
     print(x.text, file=text_file)
@@ -19,6 +22,10 @@ with open('Output.txt') as f1, open('Output_new.txt') as f2:
 #    print(l2[line])
     if l1[line] != l2[line]:
         print('Fail')
-        call(["telegram-send", message])
+        call(["/usr/local/bin/telegram-send", message])
         with open("Output.txt", "w") as text_file:
             print(x.text, file=text_file)
+
+
+
+#in cron task could be like: 30 11 * * * user ~/git/python/web_req.py https://sturm.com.ua/helikon-tex.html
