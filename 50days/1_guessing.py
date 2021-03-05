75ff0dd29854412d631445e730cd73f6104f9f4c
Serg Li <monergeim@gmail.com>
2021-03-05 18:28:39 +0200

    simplified first program

--- 50days/1_guessing.py
+++ 50days/1_guessing.py
@@ -3,11 +3,10 @@ import random as rnd
 
 def guessing_game():
     number = rnd.randint(0, 100)
-    while True:
-        n = input('Enter a number between 0 and 100 in order to guess: ')
-        if (int(n) > number):
+    while s := input('Enter a number between 0 and 100 in order to guess: '):
+        if (int(s) > number):
             print("Too big")
-        elif (int(n) < number):
+        elif (int(s) < number):
             print("Too small")
         else:
             print("Bingo")
